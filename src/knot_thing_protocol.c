8f2ef6af39cdc91617b110366714d76bb9a515e4
Rodrigo Alves <rodrigo.alves@cesar.org.br>
2016-11-04 14:06:24 -0300

    Add: Authenticate state

--- src/knot_thing_protocol.c
+++ src/knot_thing_protocol.c
@@ -113,6 +113,49 @@ static int read_register(void)
 	return 0;
 }
 
+static int send_auth(void)
+{
+	knot_msg_authentication msg;
+	knot_msg_result resp;
+	ssize_t nbytes;
+
+	memset(&msg, 0, sizeof(msg));
+
+	msg.hdr.type = KNOT_MSG_AUTH_REQ;
+	msg.hdr.payload_len = sizeof(msg.uuid) + sizeof(msg.token);
+
+	strncpy(msg.uuid, uuid, sizeof(msg.uuid));
+	strncpy(msg.token, token, sizeof(msg.token));
+
+	nbytes = hal_comm_write(-1, &msg, sizeof(msg.hdr) + msg.hdr.payload_len);
+	if (nbytes < 0)
+		return -1;
+
+	return 0;
+}
+
+static int read_auth(void)
+{
+	knot_msg_result resp;
+	ssize_t nbytes;
+
+	memset(&resp, 0, sizeof(resp));
+
+	nbytes = hal_comm_read(-1, &resp, sizeof(resp));
+
+	if (nbytes == -EAGAIN)
+		return -EAGAIN;
+
+	if (nbytes > 0) {
+		if (resp.result != KNOT_SUCCESS)
+			return -1;
+
+	} else if (nbytes < 0)
+		return nbytes;
+
+	return 0;
+}
+
 int knot_thing_protocol_run(void)
 {
 	static uint8_t state = STATE_DISCONNECTED;
@@ -150,7 +193,10 @@ int knot_thing_protocol_run(void)
 			hal_storage_read(KNOT_TOKEN_ADDR, token,
 					KNOT_PROTOCOL_TOKEN_LEN);
 
-			state = STATE_AUTHENTICATING;
+			if (send_auth() < 0)
+				state = STATE_ERROR;
+			else
+				state = STATE_AUTHENTICATING;
 		} else {
 			if (send_register() < 0)
 				state = STATE_ERROR;
@@ -158,11 +204,17 @@ int knot_thing_protocol_run(void)
 				state = STATE_REGISTERING;
 		}
 	break;
-
+	/*
+	 * Authenticating, Resgistering cases waits (without blocking)
+	 * for an response of the respective requests, -EAGAIN means there was
+	 * nothing to read so we ignore it, less then 0 an error and 0 success
+	 */
 	case STATE_AUTHENTICATING:
-		//TODO: send Auth message
-		//TODO: process Auth result
-		state = STATE_ONLINE;
+		retval = read_auth();
+		if (retval < 0)
+			state = STATE_ERROR;
+		else if (retval == 0)
+			state = STATE_SCHEMA;
 	break;
 
 	case STATE_REGISTERING:
@@ -174,9 +226,6 @@ int knot_thing_protocol_run(void)
 	break;
 
 	case STATE_SCHEMA:
-		//TODO: send schema messages
-		//TODO: process schema results
-		state = STATE_ONLINE;
 	break;
 
 	case STATE_ONLINE:
