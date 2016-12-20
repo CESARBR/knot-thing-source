d812cf08ab56180e5b3875d65cd4c139783900b6
Paulo Serra Filho <ptvsf@cesar.org.br>
2016-12-20 13:45:13 -0300

    Modify RGB LED example
    Modified RGB LED pins to PWM pins.

--- examples/Blink_RGB/Blink_RGB.ino
+++ examples/Blink_RGB/Blink_RGB.ino
@@ -9,85 +9,85 @@
 
 #include <KNoTThing.h>
 
-#define RED_LED_PIN         2
+#define RED_LED_PIN         3
 #define RED_LED_ID          1
 #define RED_LED_NAME     "Red LED"
 
-#define BLUE_LED_PIN         3
-#define BLUE_LED_ID          2
+#define GREEN_LED_PIN         5
+#define GREEN_LED_ID          2
+#define GREEN_LED_NAME     "Green LED"
+
+#define BLUE_LED_PIN         6
+#define BLUE_LED_ID          3
 #define BLUE_LED_NAME     "Blue LED"
 
-#define GREEN_LED_PIN         4
-#define GREEN_LED_ID          3
-#define GREEN_LED_NAME     "Green LED"
+
 
 KNoTThing thing;
 
-static int red_read(uint8_t *val)
+static int32_t red_led = 0;
+static int32_t green_led = 0;
+static int32_t blue_led = 0;
+
+static int red_read(int32_t *val, int32_t *multiplier)
 {
-    *val = digitalRead(RED_LED_PIN);
+    *val = red_led;
     Serial.print("RED LED: ");
-    if (*val)
-      Serial.println("ON");
-    else
-      Serial.println("OFF");
+    Serial.println(*val);
+    *multiplier = 1;
     return 0;
 }
 
-static int red_write(uint8_t *val)
+static int red_write(int32_t *val, int32_t *multiplier)
 {
-    digitalWrite(RED_LED_PIN, *val);
+    *val = *val%256;
+    red_led = *val;
+
+    analogWrite(RED_LED_PIN, *val);
     Serial.print("RED LED: ");
-    if (*val)
-      Serial.println("ON");
-    else
-      Serial.println("OFF");
+    Serial.println(*val);
       /* TODO: Save light status in EEMPROM in to handle when reboot */
     return 0;
 }
 
-static int green_read(uint8_t *val)
+static int green_read(int32_t *val, int32_t *multiplier)
 {
-    *val = digitalRead(GREEN_LED_PIN);
+    *val = green_led;
     Serial.print("GREEN LED: ");
-    if (*val)
-      Serial.println("ON");
-    else
-      Serial.println("OFF");
+    Serial.println(*val);
+    *multiplier = 1;
     return 0;
 }
 
-static int green_write(uint8_t *val)
+static int green_write(int32_t *val, int32_t *multiplier)
 {
-    digitalWrite(GREEN_LED_PIN, *val);
-    Serial.print("GREEN LED ");
-    if (*val)
-      Serial.println("ON");
-    else
-      Serial.println("OFF");
+    *val = *val%256;
+    green_led = *val;
+
+    analogWrite(GREEN_LED_PIN, *val);
+    Serial.print("GREEN LED: ");
+    Serial.println(*val);
       /* TODO: Save light status in EEMPROM in to handle when reboot */
     return 0;
 }
 
-static int blue_read(uint8_t *val)
+static int blue_read(int32_t *val, int32_t *multiplier)
 {
-    *val = digitalRead(BLUE_LED_PIN);
+    *val = blue_led;
     Serial.print("BLUE LED: ");
-    if (*val)
-      Serial.println("ON");
-    else
-      Serial.println("OFF");
+    Serial.println(*val);
+    *multiplier = 1;
     return 0;
 }
 
-static int blue_write(uint8_t *val)
+static int blue_write(int32_t *val, int32_t *multiplier)
 {
-    digitalWrite(BLUE_LED_PIN, *val);
-    Serial.print("BLUE LED ");
-    if (*val)
-      Serial.println("ON");
-    else
-      Serial.println("OFF");
+    *val = *val%256;
+    green_led = *val;
+
+    analogWrite(BLUE_LED_PIN, *val);
+    Serial.print("BLUE LED: ");
+    Serial.println(*val);
       /* TODO: Save light status in EEMPROM in to handle when reboot */
     return 0;
 }
@@ -100,15 +100,18 @@ void setup()
     pinMode(BLUE_LED_PIN, OUTPUT);
     pinMode(RED_LED_PIN, OUTPUT);
     /* TODO: Read lamp status from eeprom for reboot cases */
-    thing.init("KNoTThing");
+    thing.init("KNoTThing RGB");
 
-    thing.registerBoolData(RED_LED_NAME, RED_LED_ID, KNOT_TYPE_ID_SWITCH,
-        KNOT_UNIT_NOT_APPLICABLE, red_read, red_write);
-    thing.registerBoolData(GREEN_LED_NAME, GREEN_LED_ID, KNOT_TYPE_ID_SWITCH,
-        KNOT_UNIT_NOT_APPLICABLE, green_read, green_write);
-    thing.registerBoolData(BLUE_LED_NAME, BLUE_LED_ID, KNOT_TYPE_ID_SWITCH,
-        KNOT_UNIT_NOT_APPLICABLE, blue_read, blue_write);
+    thing.registerIntData(RED_LED_NAME, RED_LED_ID, KNOT_TYPE_ID_LUMINOSITY,
+        KNOT_UNIT_LUMINOSITY_LM, red_read, red_write);
+
+    thing.registerIntData(GREEN_LED_NAME, GREEN_LED_ID, KNOT_TYPE_ID_LUMINOSITY,
+        KNOT_UNIT_LUMINOSITY_LM, green_read, green_write);
+
+    thing.registerIntData(BLUE_LED_NAME, BLUE_LED_ID, KNOT_TYPE_ID_LUMINOSITY,
+        KNOT_UNIT_LUMINOSITY_LM, blue_read, blue_write);
     Serial.println("RGB LED KNoT Demo");
+
 }
 
 
